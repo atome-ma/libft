31a78f49b1c2be3541049952739e677c551e45ab
Antonio Tome Martin <atome-ma@c2r10s4.42malaga.com>
2022-09-23 21:34:13 +0200

    all ok for now

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,32 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atome-ma <atome-ma@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/23 19:45:22 by atome-ma          #+#    #+#             */
-/*   Updated: 2022/09/23 21:24:26 by atome-ma         ###   ########.fr       */
+/*   Created: 2022/09/23 21:12:08 by atome-ma          #+#    #+#             */
+/*   Updated: 2022/09/23 21:32:34 by atome-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stdio.h>
-# include <stddef.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-int		ft_isalpha(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_atoi(const char *str);
-char	*ft_strchr(const char *str, int ch);
-
-#endif
+char	*ft_strchr(const char *str, int ch)
+{
+	while (*str)
+	{
+		if (*str == (char) ch)
+			return ((char *)str);
+		str++;
+	}
+	if (ch == '\0')
+		return ((char *)str);
+	return (0);
+}
