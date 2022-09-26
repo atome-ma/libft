e145ed6c9d5a2035f19254cc45e0f4f2e76c8201
Antonio Tome Martin <atome-ma@c2r6s4.42malaga.com>
2022-09-26 17:34:25 +0200

    update

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: atome-ma <atome-ma@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 21:12:08 by atome-ma          #+#    #+#             */
-/*   Updated: 2022/09/23 21:32:34 by atome-ma         ###   ########.fr       */
+/*   Updated: 2022/09/26 17:27:30 by atome-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@ char	*ft_strchr(const char *str, int ch)
 {
 	while (*str)
 	{
-		if (*str == (char) ch)
+		if (*str == (unsigned char) ch)
 			return ((char *)str);
 		str++;
 	}
-	if (ch == '\0')
+	if ((unsigned char) ch == '\0')
 		return ((char *)str);
 	return (0);
 }
